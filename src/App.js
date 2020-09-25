3f42453388a5e467817d2a692768d3a69e38ccc6
paxfeline <paxfeline@gmail.com>
2020-09-25 01:23:39 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './components/logo.js';
 import './App.css';
 
 function createLogoFunctionFactory( render, execute )
 {
   return (
-    function ( ...args ) // logo function (args A)
+    function ( ...args ) // logo function
     {
       const func =
         function () // specific logo function
@@ -13,36 +13,57 @@ function createLogoFunctionFactory( render, execute )
           return execute.apply( null, func.args );
         };
       func.render = render;
-      func.setArguments =
-        function ( ...args ) { func.args = args; } // args B
-      func.setArguments( ...args ); // factory method initializes new object w/ default vals (args A)
+      func.setArguments = function ( ...args ) { func.args = args; } // different than 'args' from parent function
+      func.setArguments( ...args ); // factory method initializes new object w/ default vals
       return func;
     }
   );
 }
 
-const logoForwardFunctionFactory = createLogoFunctionFactory(
-  function ( a, b )
+const logoAddFunctionFactory = createLogoFunctionFactory(
+  function ( argumentCallback )
   {
     return (
-      <div> {a} + {b} </div>
+      <div> { argumentCallback(0) } + { argumentCallback(1) } </div>
     );
   },
   function ( a, b )
   {
-    return a + b;
+    return a() + b();
   }
 );
 
-const fwd1 = logoForwardFunctionFactory( 2, 3 );
-//fwd1.setArguments( 2, 3 );
-console.log( fwd1(), "first" );
+const logoConstantFunctionFactory = createLogoFunctionFactory(
+  function ( argumentCallback )
+  {
+    const [a,b]=useState(0);
+    return (
+      <div onClick={()=>{const n = prompt(); b(n);}}> { a } </div>
+    );
+  },
+  function ( a )
+  {
+    return a;
+  }
+);
 
-const prog = [  ];
+const const5 = logoConstantFunctionFactory( 5 );
+const const2 = logoConstantFunctionFactory( 2 );
+
+const add1 = logoAddFunctionFactory();
+add1.setArguments( const2, const5 );
+console.log( add1(), "first" );
+
+const prog = [ add1 ];
 
 function App() {
   return (
-    <Logo program={ prog } />
+    <div>
+      {
+        const5.render( n => n )
+      }
+      <Logo program={ prog } />
+    </div>
   );
 }
 
